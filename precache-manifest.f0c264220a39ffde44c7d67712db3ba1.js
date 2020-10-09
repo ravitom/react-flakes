e803b9f644e35dda8c6fac5f9ff0c0e0ef3beba3
Tom <rvi.tom@gmail.com>
2020-10-09 18:40:20 +0530

    Updates

---
+++ precache-manifest.f0c264220a39ffde44c7d67712db3ba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "103b67b2f3ecbdcd8e0c64bbb228216a",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "85926e97387e972edf68",
+    "url": "/react-project/static/css/main.2e18f275.chunk.css"
+  },
+  {
+    "revision": "bc4f0aec8753b31eee9d",
+    "url": "/react-project/static/js/2.e601ea17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-project/static/js/2.e601ea17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85926e97387e972edf68",
+    "url": "/react-project/static/js/main.5fcb0c2e.chunk.js"
+  },
+  {
+    "revision": "ebb6e51bec8001581cf6",
+    "url": "/react-project/static/js/runtime-main.68de7daf.js"
+  }
+]);
